61238d8bc0e24302b4b51b31280277b63ca381bd
M. Hopcroft <mhopeng@gmail.com>
2015-07-24 11:13:10 -0700

    display_drive_time.py v1.1

--- display_drive_time_8x8.py
+++ display_drive_time_8x8.py
@@ -6,6 +6,10 @@
 #  Hardware info at: https://learn.adafruit.com/matrix-7-segment-led-backpack-with-the-raspberry-pi/hooking-everything-up
 #
 # mhopeng@gmail.com
+#
+# v1.1 Jun2015
+#	added constants for update time
+#	startup splash is now the xy axes
 # v1.0 Jan2015
 #
 # Example URL:
@@ -31,176 +35,248 @@
 # </paths>
 #
 
+# NOTE: The 8x8 Bi-Color LED Matrix display is intended to be installed/viewed upside down
+#  from the orientation assumed by the interface library. This makes the code easier.
+
 from __future__ import division
+import os, sys, time
 # use the "requests" package for URL handling: sudo pip install requests
 import requests
 # use standard package ElementTree for XML parsing
 import xml.etree.ElementTree as ET
-import sys
-import time
-
+# use PIL for image manipulation
+import Image
+import ImageDraw
+# use AdaFruit libraries for hardware
 from Adafruit_LED_Backpack import BicolorMatrix8x8
 from Adafruit_LED_Backpack import SevenSegment
 
 # Note: pixel color values are globals:
 #   BicolorMatrix8x8.RED, BicolorMatrix8x8.GREEN, BicolorMatrix8x8.YELLOW
 
-# Start point: Daly city 280 S / CA 1 intersection
-startpoint='88'
-# Endpoint: Portola Valley, Sandhill Rd. ext from 280 S
-endpoint='1061'
+# Start point: Daly city I-280 / CA 1 intersection
+START_POINT='1473'
+# Endpoint: Menlo Park, Sandhill Rd. exit from I-280 S
+END_POINT='1061'
+# Time between updates ("x axis") in minutes
+UPDATE_INTERVAL = 3
 # estDrive is the total estimated drive time including the segments the 511 does not cover
 #  14 min to get to 280, 8 min to get from 280 to Stanford
 estOther = 14 + 8
 # Baseline value (no traffic, middle of the night)
 baseTime = 22
-# display color levels
+# bar graph color levels (in minutes)
 baselvl = 22
 greenlvl = 2
 yellowlvl = 4
 redlvl = 7
 
-print('{0}\nDisplay Commute data\n'.format(time.strftime('%A, %d %b %Y, %H:%M:%S',time.localtime())) )
+print('{0}\nCommuteClock: Display Commute data\n'.format(time.strftime('%A, %d %b %Y, %H:%M:%S',time.localtime())) )
 data_file='commute_data.csv'
 
-# initialize LED display
-# Create display instance on default I2C address (0x70) and bus number.
-display = BicolorMatrix8x8.BicolorMatrix8x8(address=0x70)
-sevenseg = SevenSegment.SevenSegment(address=0x72)
+def main():
 
-# Alternatively, create a display with a specific I2C address and/or bus.
-# display = Matrix8x8.Matrix8x8(address=0x74, busnum=1)
+	# initialize LED display
+	# Create display instance on default I2C address (0x70) and bus number.
+	display = BicolorMatrix8x8.BicolorMatrix8x8(address=0x70)
+	sevenseg = SevenSegment.SevenSegment(address=0x72)
 
-# Initialize the display(s). Must be called once before using the display.
-display.begin()
-display.clear()
-display.write_display()	
-sevenseg.begin()
-sevenseg.clear()
-sevenseg.write_display()
-# turn on the colon to show that we are working...
-sevenseg.set_colon(True)
-sevenseg.write_display()
+	# Alternatively, create a display with a specific I2C address and/or bus.
+	# display = Matrix8x8.Matrix8x8(address=0x74, busnum=1)
 
-# create 2D array for pixel values
-pxArray=[[0 for y in range(8)] for x in range(8)]
+	# Initialize the display(s). Must be called once before using the display.
+	display.begin()
+	display.clear()
+	display.write_display()	
+	sevenseg.begin()
+	sevenseg.clear()
+	sevenseg.write_display()
+
+	# turn on the colon to show that we are working...
+	sevenseg.set_colon(True)
+	sevenseg.write_display()
+	# show an image on 8x8 matrix at startup
+	#   (blatant ripoff from example file)
+	image = Image.new('RGB', (8, 8))
+	draw = ImageDraw.Draw(image)
+	#draw.rectangle((0,0,7,7), outline=(255,0,0), fill=(255,255,0))
+	# Draw an X with two green lines:
+	#draw.line((1,1,6,6), fill=(0,255,0))
+	#draw.line((1,6,6,1), fill=(0,255,0))
+	# Draw the xy axes (bottom and left):
+	draw.line((0,0,7,0), fill=(255,255,0))
+	draw.line((7,0,7,7), fill=(255,255,0))
+	# send the image to the display
+	display.set_image(image)
+	display.write_display()
+	time.sleep(2)
 
 
-try:
-	while 1:
-		# get the traffic info
-		r = requests.get('http://services.my511.org/traffic/getpathlist.aspx?token=1d19c537-9bf7-4687-bc8f-1e6e13c22ba7&o={0}&d={1}'.format(startpoint,endpoint))
+	# create 2D array for pixel values
+	pxArray=[[0 for y in range(8)] for x in range(8)]
 
-		if r.status_code != 200:
-			print('ERROR: Problem with URL request')
-			print('Response status code {0}'.format(r.status_code) )
-			sys.exit(r.status_code)
+	# create image for "error message"
+	errorImage = Image.new('RGB', (8, 8))
+	edraw = ImageDraw.Draw(errorImage)
+	# Draw an X with two red lines:
+	edraw.line((1,1,6,6), fill=(255,0,0))
+	edraw.line((1,6,6,1), fill=(255,0,0))
 
-		# r.content contains the reply XML string
-		# parse the XML into a tree
-		root = ET.fromstring(r.content)
-
-		# the xml tree contains "paths", i.e. possible routes
-		#  in this case there should be only one path
-		paths = root.findall('path')
-		if len(paths) > 1: print('WARNING: expected only 1 path, but found {0}'.format(len(paths)) )
-		if len(paths) == 0:
-			print('ERROR: no routes found')
-			sys.exit(1)
-	
-		nowTime = time.localtime()
-
-		print('{0}\nCaltrans/511 travel time for 280 S'.format(time.strftime('%A, %d %b %Y, %H:%M',nowTime)) )
-
-		# get Travel Times
-		curTime = int(paths[0].find('currentTravelTime').text)
-		print(' Current Travel Time with traffic: {0} min'.format(curTime) )
-		typTime = int(paths[0].find('typicalTravelTime').text)
-		print(' Typical Travel Time at this time: {0} min'.format(typTime) )
-
-		# get traffic incidents
-		incidents = paths[0].findall('incidents')
-		incident_list = incidents[0].findall('incident')
-		if len(incident_list) > 0:
-			print(' Traffic incidents:')
-			for incident in incident_list:
-				print('  {0}'.format(incident.text) )
-		else:
-			print(' No traffic incidents reported at this time')
+	# main loop
+	try:
+		while 1:
 		
-		## Update LED display
-		# shift the display pixel array
-		pxArray.insert(0,pxArray.pop())
-		# clear old values
-		pxArray[0] = [0 for k in range(8)]
-		# insert new values in pixel array
-		dispTime = int(max(0,curTime - baselvl)/2)
-		#print('[dispTime: {0}]'.format(dispTime) )
-		if dispTime > 0:
-			pxArray[0][0:dispTime] = [BicolorMatrix8x8.GREEN] * (dispTime)
-		if dispTime > greenlvl:
-			pxArray[0][greenlvl:dispTime] = [BicolorMatrix8x8.YELLOW] * (dispTime - greenlvl)
-		if dispTime > yellowlvl:
-			pxArray[0][yellowlvl:dispTime] = [BicolorMatrix8x8.RED] * (dispTime - yellowlvl)
+			# blank colon to show update in progress
+			sevenseg.set_colon(False)
+			sevenseg.write_display()
+			print('Requesting traffic data from CalTrans...')
 			
-		# write new pixel array
-		# Clear the display buffer.
-		display.clear()
-		# Set pixel at position i, j to appropriate color.
-		for x in range(8):
-			for y in range(8):
-				display.set_pixel(x, y, pxArray[x][y])
-		# Write the display buffer to the hardware.  This must be called to
-		# update the actual display LEDs.
-		display.write_display()		
+			# get the traffic info
+			r = requests.get('http://services.my511.org/traffic/getpathlist.aspx?token=1d19c537-9bf7-4687-bc8f-1e6e13c22ba7&o={0}&d={1}'.format(START_POINT,END_POINT))
+
+			if r.status_code != 200:
+				print('ERROR: Problem with URL request')
+				print('Response status code {0}'.format(r.status_code) )
+				# show error on display
+				display.set_image(errorImage)
+				display.write_display()
+				sys.exit(r.status_code)
+				
+			# blank colon to show update in progress
+			sevenseg.set_colon(True)
+			sevenseg.write_display()
+			
+			nowTime = time.localtime()
+
+			# r.content contains the reply XML string
+			# parse the XML into a tree
+			try:
+				root = ET.fromstring(r.content)
+			except:
+				print('ERROR: XML parse error.')
+				continue
+
+			# the xml tree contains "paths", i.e. possible routes
+			#  in this case there should be only one path
+			paths = root.findall('path')
+			if len(paths) > 1:
+				print('WARNING: received {0} possible paths. Using first in list.'.format(len(paths)) )				
+			if len(paths) == 0:
+				print('ERROR: no routes found')
+				sys.exit(1)
+				
+			# get roads in path
+			road_list = []
+			segments = paths[0].find('segments')
+			segment_list = segments.findall('segment')
+			for seg in segment_list:
+				road = seg.find('road')
+				#print('  {0}'.format(road.text) )
+				road_list.append(road.text)
+
+
+			print('{0}\nCaltrans/511 travel time for route on {1}'.format(time.strftime('%A, %d %b %Y, %H:%M',nowTime),road_list) )
+
+			# get Travel Times
+			curTime = int(paths[0].find('currentTravelTime').text)
+			print(' Current Travel Time with traffic: {0} min'.format(curTime) )
+			typTime = int(paths[0].find('typicalTravelTime').text)
+			print(' Typical Travel Time at this time: {0} min'.format(typTime) )
+
+			# get traffic incidents
+			incidents = paths[0].findall('incidents')
+			incident_list = incidents[0].findall('incident')
+			if len(incident_list) > 0:
+				print(' Traffic incidents:')
+				for incident in incident_list:
+					print('  {0}'.format(incident.text) )
+			else:
+				print(' No traffic incidents reported at this time')
+		
+		
+			## Update LED display
+			
+			# Clear the display buffer.
+			display.clear()
+		
+			# shift the display pixel array
+			pxArray.insert(0,pxArray.pop())
+			# clear old values
+			pxArray[0] = [0 for k in range(8)]
+			# insert new values in pixel array
+			dispTime = int(max(0,curTime - baselvl)/2)
+			#print('[dispTime: {0}]'.format(dispTime) )
+			if dispTime > 0:
+				pxArray[0][0:dispTime] = [BicolorMatrix8x8.GREEN] * (dispTime)
+			if dispTime > greenlvl:
+				pxArray[0][greenlvl:dispTime] = [BicolorMatrix8x8.YELLOW] * (dispTime - greenlvl)
+			if dispTime > yellowlvl:
+				pxArray[0][yellowlvl:dispTime] = [BicolorMatrix8x8.RED] * (dispTime - yellowlvl)
+			
+			# write new pixel array
+			# Set pixel at position i, j to appropriate color.
+			for x in range(8):
+				for y in range(8):
+					display.set_pixel(x, y, pxArray[x][y])
+					
+			# put a yellow box on a display if traffic incident
+			if len(incident_list) > 0:
+				for x in range(8): display.set_pixel(x, 7, BicolorMatrix8x8.YELLOW)
+			
+			# Write the display buffer to the hardware.  This must be called to
+			# update the actual display LEDs.
+			display.write_display()
 			
 		
-		# estimate arrival time
-		estDrive = estOther + curTime
-		estDriveTime = time.localtime(time.mktime(nowTime) + (60 * estDrive))
-		print(' Estimated time of Arrival: {0}'.format(time.strftime('%A, %d %b %Y, %H:%M',estDriveTime)) )
+			# estimate arrival time
+			estDrive = estOther + curTime
+			estDriveTime = time.localtime(time.mktime(nowTime) + (60 * estDrive))
+			print(' Estimated time of Arrival: {0}'.format(time.strftime('%A, %d %b %Y, %H:%M',estDriveTime)) )
 
-		# show arrival time estimate
-		#print(time.strftime('%H%M',estDriveTime))
-		sevenseg.print_number_str(time.strftime('%H%M',estDriveTime))
-		sevenseg.write_display()
+			# show arrival time estimate
+			#print(time.strftime('%H%M',estDriveTime))
+			sevenseg.print_number_str(time.strftime('%H%M',estDriveTime))
+			sevenseg.write_display()
 		
-		#commScore = (1 - ((curTime - baseTime) / baseTime))*100
-		#if commScore >= 90:
-		#	commCondition = 'green'
-		#elif commScore >= 80:
-		#	commCondition = 'yellow'
-		#elif commScore >= 70:
-		#	commCondition = 'red'
-		#else:
-		#	commCondition = 'black'
+			#commScore = (1 - ((curTime - baseTime) / baseTime))*100
+			#if commScore >= 90:
+			#	commCondition = 'green'
+			#elif commScore >= 80:
+			#	commCondition = 'yellow'
+			#elif commScore >= 70:
+			#	commCondition = 'red'
+			#else:
+			#	commCondition = 'black'
 
-		#print(' Commute Condition {0} ({1:.0f}%)'.format(commCondition,commScore) )
+			#print(' Commute Condition {0} ({1:.0f}%)'.format(commCondition,commScore) )
 		
-		f=open(data_file,'a')
-		#f.write('{0},{1},{2},{3:.0f}\n'.format(time.mktime(nowTime),curTime,typTime,commScore) )
-		f.write('{0},{1},{2}\n'.format(time.mktime(nowTime),curTime,typTime) )
-		f.close()
+			f=open(data_file,'a')
+			#f.write('{0},{1},{2},{3:.0f}\n'.format(time.mktime(nowTime),curTime,typTime,commScore) )
+			f.write('{0},{1},{2}\n'.format(time.mktime(nowTime),curTime,typTime) )
+			f.close()
 		
-		# wait for the "whole minute" between updates
-		#print(60-time.localtime()[5])
-		time.sleep(120-time.localtime()[5]) # two minutes between updates
+			# wait for the "whole minute" between updates
+			#print(60-time.localtime()[5])
+			time.sleep((UPDATE_INTERVAL*60)-time.localtime()[5])
+			print(' ')
 
 
-except KeyboardInterrupt:
-	print('** Program Stopped (INT signal) ** ' + time.strftime("%Y/%m/%d-%H:%M:%S",time.localtime()) )
-	streamEndTime=time.time() # time of stream end, unix epoch seconds
-	print('\n')
-#except:
-#	print('* Other Exception * ' + time.strftime("%Y/%m/%d-%H:%M:%S",time.localtime()) )
-#	print("Unexpected error: {0}".format(sys.exc_info()) )
+	except KeyboardInterrupt:
+		print('\n** Program Stopped (INT signal) ** ' + time.strftime("%Y/%m/%d-%H:%M:%S",time.localtime()) )
+		streamEndTime=time.time() # time of stream end, unix epoch seconds
+		print('')
+	#except:
+	#	print('* Other Exception * ' + time.strftime("%Y/%m/%d-%H:%M:%S",time.localtime()) )
+	#	print("Unexpected error: {0}".format(sys.exc_info()) )
 	
 
-#time.sleep(10)
-display.clear()
-display.write_display()	
-sevenseg.clear()
-sevenseg.write_display()	
-print('Program Exit')
+	#time.sleep(10)
+	display.clear()
+	display.write_display()	
+	sevenseg.clear()
+	sevenseg.write_display()
+	print('Program Exit')
 
-	
+
+if __name__ == "__main__":
+    main()
